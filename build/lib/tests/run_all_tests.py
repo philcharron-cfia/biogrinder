726fd8a8d49cf3a658abfad6e72d3a4a1039919f
Phil Charron <philcharron7@gmail.com>
2023-11-16 14:21:25 -0500

    removed ignore files

--- build/lib/tests/run_all_tests.py
+++
@@ -1,6 +0,0 @@
-import unittest
-
-test_loader = unittest.TestLoader()
-test_suite = test_loader.discover('.', pattern='test*.py')
-test_runner = unittest.TextTestRunner(verbosity=2)
-test_runner.run(test_suite)
